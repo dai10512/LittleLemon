a0c78c3c46c7c02360770b3c05b5988039624b81
Daisuke Osanai <dsk.osanai@gmail.com>
2024-11-17 15:51:56 +0700

    refactoring

--- littlelemon/restaurant/serializers.py
+++ littlelemon/restaurant/serializers.py
@@ -1,6 +1,5 @@
 from rest_framework import serializers
 from .models import Menu, Booking
-from rest_framework import viewsets
 from django.contrib.auth.models import User
 
 
@@ -9,10 +8,12 @@ class MenuSerializer(serializers.ModelSerializer):
         model = Menu
         fields = ['url', 'username', 'email', 'groups']
 
+
 class BookingSerializer(serializers.ModelSerializer):
     class Meta:
         model = Booking
         fields = '__all__'
+
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
