b5319521c358588eeca00b29705ca7f7ea02b730
Daisuke Osanai <dsk.osanai@gmail.com>
2024-11-18 09:03:19 +0700

    move code

--- littlelemon/LittleLemonAPI/models.py
+++ littlelemon/LittleLemonAPI/models.py
@@ -8,3 +8,24 @@ class MenuItem(models.Model):
 
     def __str__(self):
         return f'{self.title} : {str(self.price)}'
+
+
+# 移植したもの
+class Menu(models.Model):
+    id = models.AutoField(primary_key=True, unique=True, max_length=5)
+    title = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    inventory = models.IntegerField(max_length=5)
+
+    def __str__(self):
+        return f'{self.title} : {str(self.price)}'
+
+
+class Booking(models.Model):
+    id = models.AutoField(primary_key=True, unique=True, max_length=11)
+    name = models.CharField(max_length=255)
+    no_of_guests = models.IntegerField(max_length=6)
+    booking_date = models.DateField()
+
+    def __str__(self):
+        return self.name + ' - ' + str(self.booking_date)
--- littlelemon/LittleLemonAPI/serializers.py
+++ littlelemon/LittleLemonAPI/serializers.py
@@ -1,8 +1,29 @@
 from rest_framework import serializers
-from .models import MenuItem
+from .models import Booking, Menu, MenuItem
+from django.contrib.auth.models import User
 
 
 class MenuItemSerializer(serializers.ModelSerializer):
     class Meta:
         model = MenuItem
         fields = '__all__'
+
+# 移植
+
+
+class MenuSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Menu
+        fields = ['url', 'username', 'email', 'groups']
+
+
+class BookingSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Booking
+        fields = '__all__'
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['url', 'username', 'email', 'groups']
